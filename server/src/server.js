23076211df9a5eb7caaa355d53b4cad40c038352
Murshidmuhammedp <murshid.mmp@gmail.com>
2025-05-07 12:09:30 +0530

    db

--- server/src/server.js
+++ server/src/server.js
@@ -32,6 +32,6 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => { 
+app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 });   
